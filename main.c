cb72126db5676d99f848538634444ffec8e2fd36
adityapanda <adityapandaofficial@gmail.com>
2020-09-27 20:16:35 +0530

    spelling corrected by Aditya Panda

--- main.c
+++ main.c
@@ -12,7 +12,7 @@ this program tells us to "BE SPECIFIC IN WHAT WE WANT THE USER TO ENTER"
 int main()
 {
     char color[20];
-    char pulralNoun[20];
+    char pluralNoun[20];
     char celebF[20];
     char celebL[20];
 
@@ -20,13 +20,13 @@ int main()
     scanf("%s",color);
 
     printf("Enter a Plural Noun \n");
-    scanf("%s",pulralNoun);
+    scanf("%s",pluralNoun);
 
     printf("Enter a celeberity \n");
     scanf("%s %s",celebF,celebL);
 
     printf("Roses are %s \n",color);
-    printf("%s are blue \n",pulralNoun);
+    printf("%s are blue \n",pluralNoun);
     printf("I love %s %s \n And its really true!!",celebF,celebL);
 
 }
